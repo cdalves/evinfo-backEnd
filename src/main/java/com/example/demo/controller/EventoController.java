c08c01cc8f8cc6ab8f796987c73a3869e5e38b05
Carlos Daniel <cdanielalvesibce@gmail.com>
2022-10-26 20:33:15 -0300

    add bancos de dados

--- src/main/java/com/example/demo/controller/EventoController.java
+++ src/main/java/com/example/demo/controller/EventoController.java
@@ -5,9 +5,15 @@ import com.example.demo.service.EventoService;
 import com.example.demo.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +37,22 @@ public class EventoController {
     @GetMapping(path = "/{id}")
     public ResponseEntity<Evento> findById(@PathVariable long id){
         return ResponseEntity.ok(eventoService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Evento> save(@RequestBody Evento evento){
+        return new ResponseEntity<>(eventoService.save(evento), HttpStatus.CREATED); 
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id) {
+        eventoService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> replace(@RequestBody Evento evento) {
+        eventoService.replace(evento);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/com/example/demo/service/EventoService.java
+++ src/main/java/com/example/demo/service/EventoService.java
@@ -5,11 +5,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 
 @Service
 public class EventoService {
-    private List<Evento> eventos = List.of(new Evento(1L, 123L, "Concerto de música classica", "Concerto com apresentação de peças dos maiores compositores", "30/10/2022"), new Evento(2L, 321L, "Exposição de artes", "Exposição de artes para artistas regionais", "07/11/2022"));
+    private static List<Evento> eventos;
+
+    static{
+        eventos = new ArrayList<>(List.of(new Evento(1L, 123L, "Concerto de música classica", "Concerto com apresentação de peças dos maiores compositores", "30/10/2022"), new Evento(2L, 321L, "Exposição de artes", "Exposição de artes para artistas regionais", "07/11/2022")));
+    }
 
     // private final AnimeRepository animeRepository;
     public List<Evento> listAll() {
@@ -21,5 +28,20 @@ public class EventoService {
                 .filter(evento -> evento.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Evento não encontrado"));
+    }
+
+    public Evento save(Evento evento){
+        evento.setId(ThreadLocalRandom.current().nextLong(3, 100000));
+        eventos.add(evento);
+        return evento;
+    }
+
+    public void delete(long id) {
+        eventos.remove(findById(id));
+    }
+
+    public void replace(Evento evento) {
+        delete(evento.getId());
+        eventos.add(evento);
     }
 }
