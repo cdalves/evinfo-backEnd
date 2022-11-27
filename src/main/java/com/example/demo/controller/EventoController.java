8c8784b558c6c52496c7c07f9a1d223990800cf6
Carlos Daniel <cdanielalvesibce@gmail.com>
2022-11-27 12:08:09 -0300

    fix conect front

--- src/main/java/com/example/demo/controller/EventoController.java
+++ src/main/java/com/example/demo/controller/EventoController.java
@@ -12,6 +12,7 @@ import lombok.extern.log4j.Log4j2;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,8 +26,9 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
-@RequestMapping("eventos")
+@RequestMapping("/eventos")
 @Log4j2
 @RequiredArgsConstructor
 public class EventoController {
